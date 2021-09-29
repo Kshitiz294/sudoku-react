2836dff2318a774192482bd46ccd123f21e20c99
Kshitiz Gupta <kshitiz.gupta2@keysight.com>
2021-09-29 22:53:36 +0530

    creating sudoku board

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import * as sudoku from './generator';
+import { Cube } from './models/cube';
 
 function App() {
-  const [gameBoard, setGameBoard] = useState<Array<number[]>>([]);
+  const [gameBoard, setGameBoard] = useState<Array<Cube[]>>([]);
   const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   useEffect(() => {
-    const board: Array<number[]> = [];
-    for(let i = 0; i < 9; i++) {
-      let row: number[] = [];
-      for (let j = 0; j < 9; j++) {
-        row.push(0);
+    let solution: number[][];
+    // Iteratively generate a sudoku solution
+    while (true) {
+      try {
+        solution = sudoku.generateSudokuSolution();
+        break;
+  
+      } catch (err: any) {
+        // Nothing required
       }
-      board.push(row);
+    }
+
+    // We have a solution, let's add it to the board
+    const board: Cube[][] = solution.map((row: number[]) => {
+      return row.map((n: number) => {
+        return { reality: n, readonly: false };
+      });
+    });
+
+    // let's show a few numbers (17 to be precise)
+    let count = 0;
+    while (count <= 17) {
+      const rowIndex = Math.floor(Math.random() * 8);
+      const columnIndex = Math.floor(Math.random() * 8);
+
+      if(!board[rowIndex][columnIndex].readonly) {
+        board[rowIndex][columnIndex].readonly = true;
+        board[rowIndex][columnIndex].value = board[rowIndex][columnIndex].reality;
+        count += 1;
+      }
     }
 
     setGameBoard(board);
-
-    // Testing sudoku generator
-    let successfulSolutions = 0;
-    let unsuccessfuleSolutions = 0;
-    const sols: Array<number[][]> = [];
-
-    for (let i = 0; i < 1000; i++) {
-      try {
-        let dummy = sudoku.generateSudokuSolution();
-        successfulSolutions += 1;
-        sols.push(dummy);
-  
-      } catch (err: any) {
-        unsuccessfuleSolutions += 1;
-        // console.log(err.message);
-      }
-    }
-
-
-    console.log('successful', successfulSolutions);
-    console.log('unsuccessful', unsuccessfuleSolutions);
-    console.log(sols);
+    
   }, []);
 
   return (
@@ -46,12 +49,12 @@ function App() {
 
       {/* Game Board */}
       {
-        gameBoard.map((row: number[]) => {
+        gameBoard.map((row: Cube[], i: number) => {
           return (
-            <div className="flex-container-row">
+            <div key={i} className="flex-container-row">
               {
-                row.map((n: number) => {
-                  return <div className="cube flex-container-column"></div>
+                row.map((cube: Cube, j: number) => {
+                  return <div key={j} className="cube flex-container-column">{cube.readonly ? cube.reality : ''}</div>
                 })
               }
             </div>
@@ -62,8 +65,8 @@ function App() {
       {/* Buttons */} 
       <div className="flex-container-row" style={{marginTop: '20px', width: '288px', justifyContent: 'space-between'}}>
         {
-          numberButtons.map((num: number) => {
-            return <button>{num}</button>
+          numberButtons.map((num: number, index: number) => {
+            return <button key={index}>{num}</button>
           })
         }
       </div>
---
+++ src/models/cube.ts
@@ -0,0 +1,5 @@
+export interface Cube {
+    value?: number;
+    reality: number;
+    readonly: boolean;
+}
