bc2260ac147592570f3d9c553686894babc0a9bc
Kshitiz Gupta <kshitiz.gupta2@keysight.com>
2021-09-30 15:51:52 +0530

    Basic game mechanics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Cube } from './models/cube';
 
 function App() {
   const [gameBoard, setGameBoard] = useState<Array<Cube[]>>([]);
+  const [selectedCube, setSelectedCube] = useState<Cube | undefined>(undefined);
   const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   useEffect(() => {
@@ -27,9 +28,9 @@ function App() {
       });
     });
 
-    // let's show a few numbers (17 to be precise)
+    // let's show a few numbers (30 to be precise)
     let count = 0;
-    while (count <= 17) {
+    while (count <= 30) {
       const rowIndex = Math.floor(Math.random() * 8);
       const columnIndex = Math.floor(Math.random() * 8);
 
@@ -44,6 +45,18 @@ function App() {
     
   }, []);
 
+  const selectCube = (rowIndex: number, columnIndex: number) => {
+    setSelectedCube(gameBoard[rowIndex][columnIndex].readonly ? undefined : gameBoard[rowIndex][columnIndex]);
+  }
+
+  const setNumberInCube = (n: number) => {
+    if (selectedCube) {
+      selectedCube.value = n;
+      const board = [...gameBoard];
+      setGameBoard(board);
+    }
+  }
+
   return (
     <div className="App flex-container-column">
 
@@ -54,7 +67,7 @@ function App() {
             <div key={i} className="flex-container-row horizontal-row">
               {
                 row.map((cube: Cube, j: number) => {
-                  return <div key={j} className="cube flex-container-column">{cube.readonly ? cube.reality : ''}</div>
+                  return <div key={j} className="cube flex-container-column" onClick={() => selectCube(i, j)}>{cube.readonly ? cube.reality : cube.value}</div>
                 })
               }
             </div>
@@ -66,7 +79,7 @@ function App() {
       <div className="flex-container-row" style={{marginTop: '20px', width: '288px', justifyContent: 'space-between'}}>
         {
           numberButtons.map((num: number, index: number) => {
-            return <button key={index}>{num}</button>
+            return <button key={index} disabled={selectedCube === undefined} onClick={() => setNumberInCube(num)}>{num}</button>
           })
         }
       </div>
